8cfd4c09ababdc76b4cb46334af5d4a4e08a6314
Manuel <manuelmtz_25@hotmail.com>
2021-06-04 00:33:34 -0500

    environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
+import { environments } from './environments';
+
 @Module({
   imports: [
     HttpModule,
@@ -13,7 +15,7 @@ import { DatabaseModule } from './database/database.module';
     ProductsModule,
     DatabaseModule,
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
     }),
   ],
